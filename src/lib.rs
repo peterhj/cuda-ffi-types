ab7095eec918e4677372fce87fbb4c285e927d89
Peter Jin <peterhaijin@gmail.com>
2018-12-25 18:46:33 -0800

    Add static version checks. Fix versions of dependent crates.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 
+#[macro_use] extern crate static_assertions;
+
 pub mod cuda {
 include!(concat!(env!("OUT_DIR"), "/cuda.rs"));
 }
@@ -18,4 +20,30 @@ include!(concat!(env!("OUT_DIR"), "/driver_types.rs"));
 
 pub mod library_types {
 include!(concat!(env!("OUT_DIR"), "/library_types.rs"));
+}
+
+mod version_checks {
+  #[cfg(feature = "cuda_6_5")]  const_assert_eq!(cuda_api_version; crate::cuda::__CUDA_API_VERSION,  6050);
+  #[cfg(feature = "cuda_6_5")]  const_assert_eq!(cuda_version;     crate::cuda::CUDA_VERSION,        6050);
+
+  #[cfg(feature = "cuda_7_0")]  const_assert_eq!(cuda_api_version; crate::cuda::__CUDA_API_VERSION,  7000);
+  #[cfg(feature = "cuda_7_0")]  const_assert_eq!(cuda_version;     crate::cuda::CUDA_VERSION,        7000);
+
+  #[cfg(feature = "cuda_7_5")]  const_assert_eq!(cuda_api_version; crate::cuda::__CUDA_API_VERSION,  7050);
+  #[cfg(feature = "cuda_7_5")]  const_assert_eq!(cuda_version;     crate::cuda::CUDA_VERSION,        7050);
+
+  #[cfg(feature = "cuda_8_0")]  const_assert_eq!(cuda_api_version; crate::cuda::__CUDA_API_VERSION,  8000);
+  #[cfg(feature = "cuda_8_0")]  const_assert_eq!(cuda_version;     crate::cuda::CUDA_VERSION,        8000);
+
+  #[cfg(feature = "cuda_9_0")]  const_assert_eq!(cuda_api_version; crate::cuda::__CUDA_API_VERSION,  9000);
+  #[cfg(feature = "cuda_9_0")]  const_assert_eq!(cuda_version;     crate::cuda::CUDA_VERSION,        9000);
+
+  #[cfg(feature = "cuda_9_1")]  const_assert_eq!(cuda_api_version; crate::cuda::__CUDA_API_VERSION,  9010);
+  #[cfg(feature = "cuda_9_1")]  const_assert_eq!(cuda_version;     crate::cuda::CUDA_VERSION,        9010);
+
+  #[cfg(feature = "cuda_9_2")]  const_assert_eq!(cuda_api_version; crate::cuda::__CUDA_API_VERSION,  9020);
+  #[cfg(feature = "cuda_9_2")]  const_assert_eq!(cuda_version;     crate::cuda::CUDA_VERSION,        9020);
+
+  #[cfg(feature = "cuda_10_0")] const_assert_eq!(cuda_api_version; crate::cuda::__CUDA_API_VERSION, 10000);
+  #[cfg(feature = "cuda_10_0")] const_assert_eq!(cuda_version;     crate::cuda::CUDA_VERSION,       10000);
 }
